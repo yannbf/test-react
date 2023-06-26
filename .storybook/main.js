5c56db1b88b11dda271eba57272aff4ca80c9b8d
Yann Braga <yannbf@gmail.com>
2023-06-27 00:41:04 +0200

    set up onboarding

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,21 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
+    name: '@storybook/react-webpack5',
+    options: {},
   },
   docs: {
-    autodocs: "tag"
+    autodocs: 'tag',
   },
-  staticDirs: ["../public"]
-};
-export default config;
+  staticDirs: ['../public'],
+}
+export default config
